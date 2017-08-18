04fd4ab0dd77c058a4fe73f0f4d3cdf3d81f746e
liuerwal <liuerwal@gmail.com>
2017-08-18 18:50:36 +0800

    tab bar component

--- comps/tabBar/index.js
+++ comps/tabBar/index.js
@@ -1,12 +1,52 @@
+var _ = require('../../asset/js/util');
 
 module.exports = {
     data: {
-        a: 'a',
-        deep: {
-            a: 1
-        }
+        
+    },
+    customData: {
+        tabBar: 'home',
+        tabName: [
+            'home',
+            'message',
+            'mine',
+        ],
     },
     onLoad: function(){
-        console.log('tab bar')
+
+        this.setTabBar();
+
+    },
+
+    switchTo: function(e){
+        var user = _.cache('user')
+        var tab = e.currentTarget.dataset.tab
+
+        if ( tab === 'mine' ){
+            _.redirectTo('/pages/mime/index')
+
+        }else if ( tab === 'message' ){
+            _.redirectTo('/pages/message/index')
+
+        }else if ( tab === 'home' ){
+            if ( user.role == 'buyer' ){
+                _.redirectTo('/pages/order/index')
+
+            }else if( user.role == 'driver'){
+                _.redirectTo('/pages/order/list')
+            }
+        }
+    },
+
+    setTabBar: function(tab){
+        var icon = [] , _this = this;
+
+        this.customData.tabName.forEach(function(name){
+            icon.push('../../asset/img/'+name+(_this.customData.tabBar==name ? '-a' : '')+'.png');
+        });
+
+        this.setData({
+            tabIcon: icon
+        });
     }
 }
